92f42e1a81d319201d6ef5b16c38e97672ce59d6
Pablo Fonseca <pablofonseca777@gmail.com>
2016-09-17 00:37:52 -0300

    Finish triangle_project_2

--- triangle.rb
+++ triangle.rb
@@ -14,11 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if invalid_params([a, b, c])
+
   case [a,b,c].uniq.size
   when 1 then :equilateral
   when 2 then :isosceles
   else :scalene
   end
+end
+
+private
+
+def invalid_params(params)
+  a, b, c = params.sort
+  a <= 0 || a + b <= c
 end
 
 # Error class used in part 2.  No need to change this code.
